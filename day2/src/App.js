25c3cdb106020c6f04c78d0e135aea3054f1f07c
ShravaniMishra <shravniguddi@gmail.com>
2022-10-18 23:10:38 +0530

    added day-2

---
+++ day2/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Button from "./components/Button"
+
+function App() {
+  return (
+    <div className="App">
+     <Button text="Click Me!"/>
+    </div>
+  );
+}
+
+export default App;
