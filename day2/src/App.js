63ef348ace8309235ac4416567fff798ea51af9c
ShravaniMishra <shravniguddi@gmail.com>
2022-10-19 01:12:59 +0530

    added day-2

--- day2/src/App.js
+++ day2/src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
-import Button from "./components/Button"
+import Button from "./components/Button";
+import Avatar from "./components/Avatar";
 
 function App() {
   return (
     <div className="App">
      <Button text="Click Me!"/>
+     <Avatar/>
     </div>
   );
 }
---
+++ day2/src/components/Avatar.jsx
@@ -0,0 +1,8 @@
+function Avatar(){
+    return(
+        <div>
+            <img src="https://avatars.githubusercontent.com/u/107975234?v=4" alt="" />
+        </div>
+    )
+}
+export default Avatar;
