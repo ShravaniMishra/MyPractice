9e520c9d36028e1e7c33fe4ff9b8347b90cf3649
ShravaniMishra <shravniguddi@gmail.com>
2022-10-19 18:00:25 +0530

    added day-3

--- day3/src/components/Counter.jsx
+++ day3/src/components/Counter.jsx
@@ -5,7 +5,9 @@ function Counter(){
     return(
         <div>
             Counter: {count}
-            
+            <button onClick={()=>setCount(count+1)}>Add</button>
+            <button disabled={count==0} onClick={()=>setCount(count-1)}>Reduce</button>
+            <button onClick={()=>setCount(0)}>RESET</button>
         </div>
     )
 }
