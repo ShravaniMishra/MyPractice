221cf04c6a0caba20fc917fe46ff6afa6037fed3
ShravaniMishra <shravniguddi@gmail.com>
2023-02-28 19:39:24 +0530

    added Date and Time

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+
+function App() {
+  
+  const name = "Shravani Mishra"
+  const currDate = new Date().toLocaleDateString()
+  const currTime = new Date().toLocaleTimeString()
+
+  return (
+    <div className="App">
+      <>
+      <h1>My Name is {name}</h1>
+      <h2>Current Date : {currDate}</h2>
+      <h2>Current Time : {currTime}</h2>
+      </>
+    </div>
+  )
+}
+
+export default App
