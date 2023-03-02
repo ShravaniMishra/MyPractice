e001575076817142a19d90e16cb78d366a5f05bd
ShravaniMishra <shravniguddi@gmail.com>
2023-03-02 15:31:02 +0530

    worked by map method

---
+++ mini-project/src/Data.jsx
@@ -0,0 +1,47 @@
+const data = [
+  {
+    id:1,
+    imgsrc:
+      "https://m.media-amazon.com/images/S/pv-target-images/447febede47ca1b15b4aa3da4d8542882b4080774371bacc62292c602c82e50a._UR1920,1080_SX356_FMwebp_.jpg",
+    title: "A PrimeVideo Original Movie",
+    fname: "Raabta",
+    link: "https://www.primevideo.com/region/eu/detail/0U8EQJFD44MXI7EJESCZPFH7Z7/ref=atv_dp_atf_prime_sd_mv_resume_t1ADAAAAAA0wg0?autoplay=1&t=2945",
+  },
+  {
+    id:2,
+    imgsrc:"https://m.media-amazon.com/images/S/pv-target-images/cb63b2bf1e1e4df8aeb7912fb5029a8a8e82a36e0c28f512199975f7a62ae112._UR1920,1080_SX356_FMwebp_.jpg",
+      title:"A PrimeVideo Original Movie",
+      fname:"Sita Ramam",
+      link:"https://www.primevideo.com/region/eu/detail/0G2G9F9BZAJUW8XZ2RQ5N3MRBS/ref=atv_hm_hom_1_c_cjm7wb_2_2"
+  },
+  {
+    id:3,
+    imgsrc:"https://m.media-amazon.com/images/S/pv-target-images/967358deda2aa88c2404a13b41ff827c49dfacd7e3f64ccfb77a30926df8fb24._UR1920,1080_SX356_FMwebp_.jpg",
+      title:"A Horror Movie",
+      fname:"THE EXORCIST",
+      link:"https://www.primevideo.com/region/eu/detail/0TVLYOHBN2E7AIOOAV65ZQDQR9/ref=atv_me_ren_c_TpGd0l_brws_5_1"
+  },
+  {
+    id:4,
+    imgsrc:"https://m.media-amazon.com/images/S/pv-target-images/fe9f6e33ffd0d9ce936a53a856228457f1e42fbd37327dbc5fbfd5f567003985._UR1920,1080_SX356_FMwebp_.jpg",
+      title:"Loveable Movie",
+      fname:"Charlie 777",
+      link:"https://www.primevideo.com/region/eu/detail/0IVI12HKR929M9BRHUUTBLE34Z/ref=atv_me_ren_c_kDLprB_brws_10_1"
+  },
+  {
+    id:5,
+    imgsrc:"https://m.media-amazon.com/images/S/pv-target-images/c5e1ffea964b645052c3101cf403868a721da5f34366c515fde9306be8d7e54a._UR1920,1080_SX356_FMwebp_.jpg",
+    title:"A PrimeVideo Original Series",
+    fname:"Mirzapur",
+    link:"https://www.primevideo.com/region/eu/detail/0PDOKMV9CRLOMO5EUKNCUJLG4Q/ref=atv_hm_hom_1_c_cjm7wb_2_3g"
+  },
+  {
+    id:6,
+    imgsrc:"https://m.media-amazon.com/images/S/pv-target-images/38e1d919fd47753765ed85c092eea78b23b974650fa12ac05c479c56fb7acf44._UR1920,1080_SX356_FMwebp_.jpg",
+    title:"A PrimeVideo Original Series",
+    fname:"Minus One",
+    link:"https://www.primevideo.com/region/eu/detail/0L1Z59VW50BY9GRQZKRJIRJBTZ/ref=atv_hm_hom_1_c_g88SS5_brws_6_2"
+  }
+];
+
+export default data;
